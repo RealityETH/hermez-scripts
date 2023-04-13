b9f65de1e235be5a615dab56e0dc37c4c92dbd7f
Edmund Edgar <ed@socialminds.jp>
2023-04-13 08:00:25 +0000

    fix config path, prune docker containers to avoid errors when they already exist

--- prod.sh
+++ prod.sh
@@ -1,12 +1,15 @@
 #ZKEVM_NET=mainnet
 ZKEVM_NET=testnet
 ZKEVM_DIR=/var/lib/hermez # CHANGE THIS
-ZKEVM_CONFIG_DIR=./etc/hermez# CHANGE THIS
+ZKEVM_CONFIG_DIR=/etc/hermez# CHANGE THIS
 
 mkdir -p $ZKEVM_CONFIG_DIR
 mkdir -p $ZKEVM_DIR
 mkdir -p "$ZKEVM_DIR/$ZKEVM_NET"
 mkdir -p "$ZKEVM_DIR/data"
+
+# Make sure nothing conflicts
+docker container prune
 
 curl -L https://github.com/0xPolygonHermez/zkevm-node/releases/latest/download/$ZKEVM_NET.zip > $ZKEVM_NET.zip && unzip -o $ZKEVM_NET.zip -d $ZKEVM_DIR && rm $ZKEVM_NET.zip
 cp ./hermez.env $ZKEVM_CONFIG_DIR/.env
