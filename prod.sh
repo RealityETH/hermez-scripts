ebbea00c7eac9377361abd823d0592b7ea4cd5ec
Edmund Edgar <ed@socialminds.jp>
2023-04-14 05:22:11 +0000

    handle repeated downloads better etc

--- prod.sh
+++ prod.sh
@@ -4,16 +4,25 @@
 ZKEVM_NET=testnet
 ZKEVM_DIR=/var/lib/hermez
 ZKEVM_CONFIG_DIR=/etc/hermez
+#ZKEVM_ADVANCED_CONFIG_DIR=/etc/hermez-advanced
+
+# Make sure nothing conflicts
+docker container prune -f
+rm -rf $ZKEVM_DIR
 
 mkdir -p $ZKEVM_CONFIG_DIR
+mkdir -p $ZKEVM_ADVANCED_CONFIG_DIR
 mkdir -p $ZKEVM_DIR
 mkdir -p "$ZKEVM_DIR/$ZKEVM_NET"
 mkdir -p "$ZKEVM_DIR/data"
 
-# Make sure nothing conflicts
-docker container prune -f
+if [ -F $FILE ]; then
+   #curl -L https://github.com/0xPolygonHermez/zkevm-node/releases/latest/download/$ZKEVM_NET.zip > $ZKEVM_NET.zip && unzip -o $ZKEVM_NET.zip -d $ZKEVM_DIR && rm $ZKEVM_NET.zip
+   curl -L https://github.com/0xPolygonHermez/zkevm-node/releases/latest/download/$ZKEVM_NET.zip > $ZKEVM_NET.zip
+fi
 
-curl -L https://github.com/0xPolygonHermez/zkevm-node/releases/latest/download/$ZKEVM_NET.zip > $ZKEVM_NET.zip && unzip -o $ZKEVM_NET.zip -d $ZKEVM_DIR && rm $ZKEVM_NET.zip
+unzip -o $ZKEVM_NET.zip -d $ZKEVM_DIR 
+
 cp ./hermez.env $ZKEVM_CONFIG_DIR/.env
 
 # RUN:
