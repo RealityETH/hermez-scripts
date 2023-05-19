47bc244e2991a7fec0b8a851e4fb89795db73d34
Edmund Edgar <ed@socialminds.jp>
2023-05-19 08:19:41 +0000

    exit early, fix broken filename check

--- prod.sh
+++ prod.sh
@@ -10,25 +10,31 @@ ZKEVM_CONFIG_DIR=/etc/hermez
 docker container prune -f
 rm -rf $ZKEVM_DIR
 
+sudo rmdir -rf $ZKEVM_DIR;
+
 mkdir -p $ZKEVM_CONFIG_DIR
 mkdir -p $ZKEVM_ADVANCED_CONFIG_DIR
 mkdir -p $ZKEVM_DIR
 mkdir -p "$ZKEVM_DIR/$ZKEVM_NET"
 mkdir -p "$ZKEVM_DIR/data"
 
-if [ -F $FILE ]; then
+if [ -F $ZKEVM_NET]; then
    #curl -L https://github.com/0xPolygonHermez/zkevm-node/releases/latest/download/$ZKEVM_NET.zip > $ZKEVM_NET.zip && unzip -o $ZKEVM_NET.zip -d $ZKEVM_DIR && rm $ZKEVM_NET.zip
    curl -L https://github.com/0xPolygonHermez/zkevm-node/releases/latest/download/$ZKEVM_NET.zip > $ZKEVM_NET.zip
 fi
 
 unzip -o $ZKEVM_NET.zip -d $ZKEVM_DIR 
 
+exit
 cp ./hermez.env $ZKEVM_CONFIG_DIR/.env
 
+
+exit
 # RUN:
 docker compose --env-file $ZKEVM_CONFIG_DIR/.env -f $ZKEVM_DIR/$ZKEVM_NET/docker-compose.yml up -d
 
 
+exit
 # put the db migration script somewhere we can read it
 sudo cp ./zkevm-node/db/scripts/single_db_server.sql /var/lib/hermez/data/init_db.sql
 # Edited to add user/db creation
