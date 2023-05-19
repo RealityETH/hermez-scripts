ea48688e365a817e2bdd93eb9e579d3a7d7dad5e
Edmund Edgar <ed@socialminds.jp>
2023-05-19 02:31:13 +0000

    db setup steps

--- prod.sh
+++ prod.sh
@@ -27,3 +27,16 @@ cp ./hermez.env $ZKEVM_CONFIG_DIR/.env
 
 # RUN:
 docker compose --env-file $ZKEVM_CONFIG_DIR/.env -f $ZKEVM_DIR/$ZKEVM_NET/docker-compose.yml up -d
+
+
+# put the db migration script somewhere we can read it
+sudo cp ./zkevm-node/db/scripts/single_db_server.sql /var/lib/hermez/data/init_db.sql
+# Edited to add user/db creation
+
+sudo apt install postgresql
+# edit postgresql.conf and pg_hba to allow the docker address
+# todo: check if we need this
+
+sudo su - postgres
+psql < /var/lib/hermez/data/init_db.sql 
+exit
